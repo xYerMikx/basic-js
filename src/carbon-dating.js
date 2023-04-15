27386052b2d4e0ac9562eb29bfc2f066ee3e8728
xYerMikx <yermik2014@yandex.ru>
2023-04-15 12:55:06 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || typeof sampleActivity !== 'number') return false
+  const num = Number(sampleActivity)
+  if (sampleActivity === null || sampleActivity === undefined) return false
+  if (isNaN(num)) return false
+  return Math.ceil(Math.log(MODERN_ACTIVITY / num) * HALF_LIFE_PERIOD / 0.693)
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,8 +11,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  throw new NotImplementedError("Nope")
   // remove line with error and write your code here
 }
 
