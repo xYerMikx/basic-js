755dc448d425252e50f2dcb07ec18fec19e7cb9e
xYerMikx <yermik2014@yandex.ru>
2023-04-18 02:54:41 +0300

    feat: solve all problems

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt(msg, key) {
-    if (!msg || !key) {
-      throw new Error('Incorrect arguments!');
-    }
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt(encryptedMsg, key) {
 
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let messageIndex = this.alphabet.indexOf(message[i]);
+        let keyIndexMod = keyIndex % key.length;
+        let keyLetterIndex = this.alphabet.indexOf(key[keyIndexMod]);
+        let encryptedIndex = (messageIndex + keyLetterIndex) % this.alphabet.length;
+        result += this.alphabet[encryptedIndex];
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        let encryptedIndex = this.alphabet.indexOf(encryptedMessage[i]);
+        let keyIndexMod = keyIndex % key.length;
+        let keyLetterIndex = this.alphabet.indexOf(key[keyIndexMod]);
+        let decryptedIndex =
+          (encryptedIndex - keyLetterIndex + this.alphabet.length) %
+          this.alphabet.length;
+        result += this.alphabet[decryptedIndex];
+        keyIndex++;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
 
